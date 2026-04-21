f8a8b7383fa536dc9bcba0284437f2866467ec7a
Alberto Valdez <albertovaldez434@gmail.com>
2026-04-21 14:57:41 -0700

    se agrego el jwt helper para revisar la expiracion del token con types ya definidos

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,14 +1,20 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt'
 import { AuthUser } from '../models/users';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  private jwtHelper = new JwtHelperService;
+
+  checkTokenExpired(tokenString: string): boolean {
+    return this.jwtHelper.isTokenExpired(tokenString);
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('AuthInterceptor: intercepting request');
-    
+
     const dataLogin = localStorage.getItem('loginData');
     let storedLogin: AuthUser | null = null;
 
@@ -16,8 +22,15 @@ export class AuthInterceptor implements HttpInterceptor {
       storedLogin = JSON.parse(dataLogin);
     }
 
-
     if (storedLogin?.access_token) {
+      const tokenExp = this.checkTokenExpired(storedLogin.access_token);
+
+      if (tokenExp) {
+        console.log('token expirado');
+        localStorage.removeItem('loginData');
+        return next.handle(req);
+      }
+      
       req = req.clone({
         setHeaders: {
           Authorization: `Bearer ${storedLogin.access_token}`,
--- src/app/users-guard-guard.ts
+++ src/app/users-guard-guard.ts
@@ -2,24 +2,33 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UsuariosService } from './services/usuarios';
 import { map, take } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 export const usersGuardGuard: CanActivateFn = (route, state) => {
 
   const authService = inject(UsuariosService);
   const router = inject(Router);
+  const jwtHelper = new JwtHelperService;
 
   return authService.LoginData$.pipe(
-    take(1), 
+    take(1),
     map(loginData => {
       console.log('Login data in guard:', loginData); // Agrega este log para verificar el valor de loginData
-      if (loginData?.idUser) {
-        return true; // Permitir acceso si el usuario está autenticado
-      } else {
-        return router.createUrlTree(
-          ['/login'], // Redirigir al usuario a la página de inicio de sesión 
-          { queryParams: { returnUrl: state.url } } // Opcional: pasar la URL de retorno para redirigir después del inicio de sesión
-        );
+
+      const token = loginData?.access_token;
+
+      if (token && !jwtHelper.isTokenExpired(token)) {
+        return true;
       }
+
+      console.log('token in login data expired'); // Agrega este log para verificar el valor de loginData
+      localStorage.removeItem('loginData');
+
+      return router.createUrlTree(
+        ['/login'], // Redirigir al usuario a la página de inicio de sesión 
+        { queryParams: { returnUrl: state.url } } // Opcional: pasar la URL de retorno para redirigir después del inicio de sesión
+      );
+
     })
   );
 
