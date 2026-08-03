37f4d8c8822ea88e81da80d70961fa84d209fb3e
Alberto Valdez <albertovaldez434@gmail.com>
2026-08-03 10:55:58 -0700

    - pruebas de crypto service completadas

---
+++ src/app/tests/Services/crypto.spec.ts
@@ -0,0 +1,64 @@
+import { TestBed } from "@angular/core/testing";
+import { Crypto } from "src/app/services/crypto"
+
+describe('Crypto Test', () => {
+    let cryptService: Crypto;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [cryptService]
+        })
+
+        cryptService = TestBed.inject(Crypto);
+    });
+
+    it('Should be created', () => {
+        expect(cryptService).toBeTruthy();
+    })
+
+    it('Deberia de encriptar datos', async () => {
+        let text = 'Esto es un texto';
+        let password = 'MyP4ssw0rd@';
+
+        const info = await cryptService.encrypt(text, password);
+
+        expect(cryptService.encrypt).toHaveBeenCalledWith(text,password);
+
+        expect(info.data).toBeTruthy();
+        expect(info.iv).toBeTruthy();
+        expect(info.salt).toBeTruthy();
+    });
+
+    it('Deberia desencriptar datos', async () => {
+        let text = 'Esto es un texto';
+        let password = 'MyP4ssw0rd@';
+
+        const info = await cryptService.encrypt(text, password);
+
+        expect(cryptService.encrypt).toHaveBeenCalledWith(text,password);
+
+        expect(info.data).toBeTruthy();
+        expect(info.iv).toBeTruthy();
+        expect(info.salt).toBeTruthy();
+
+        const decryptInfo = await cryptService.decrypt(info, password);
+
+        expect(cryptService.decrypt).toHaveBeenCalledWith(info, password);
+
+        expect(decryptInfo).toBeTruthy();
+        expect(decryptInfo).toBeGreaterThan(0);
+    });
+
+    it('Deberia de generar una cryptokey', async() => {
+        let password = 'MyP4ssw0rd@';
+        const saltArray = new Uint8Array([1, 2, 3, 4, 5]);
+        
+        const key = await cryptService.generarLlave(password, saltArray);
+
+        expect(cryptService.generarLlave).toHaveBeenCalledWith(password, saltArray);
+
+        expect(key).toBeTruthy();
+        expect(key.type).toBe('secret');
+        expect(key.algorithm.name).toBe('AES-GCM');
+    });
+})
